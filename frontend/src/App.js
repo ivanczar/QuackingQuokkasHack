9e7d57610b6c8a5fad572323838e4fc7c81ef2c2
Jacob Tupe <jadatu13@outlook.com>
2022-09-30 11:34:13 +1300

    Update styles of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import ScanScreen from './pages/ScanScreen';
-import Navbar from './components/Navbar';
-import RegisterScreen from './pages/RegisterScreen';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import "./App.css";
+import ScanScreen from "./pages/ScanScreen";
+import Navbar from "./components/Navbar";
+import RegisterScreen from "./pages/RegisterScreen";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
-  
   return (
     <Router>
-    
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/scan' element={<ScanScreen />} />
-        <Route path='/register' element={<RegisterScreen />} />
+        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path="/scan" element={<ScanScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
       </Routes>
     </Router>
   );
