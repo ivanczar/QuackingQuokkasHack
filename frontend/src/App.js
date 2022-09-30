ee268e65d180a508f385d03347ac86e4b7d8d9eb
Ivan <ivanczar2013@gmail.com>
2022-09-30 22:17:35 +1300

    wentbackoops

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import "./App.css";
-import ScanScreen from "./pages/ScanScreen";
-import Navbar from "./components/Navbar";
-import RegisterScreen from "./pages/RegisterScreen";
-import PageNotFound404 from  "./pages/PageNotFound404";
+import './App.css';
+import ScanScreen from './pages/ScanScreen';
+import Navbar from './components/Navbar';
+import RegisterScreen from './pages/RegisterScreen';
+import PageNotFound404 from './pages/PageNotFound404';
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-} from "react-router-dom";
+} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/register" />} />
-        <Route path="/scan/:id" element={<ScanScreen />} />
-        <Route path="/register" element={<RegisterScreen />} />
-        {/* <Route path="*" element={<PageNotFound404 />} /> */}
+        <Route path='/' element={<Navigate to='/register' />} />
+        <Route path='/scan' element={<ScanScreen />} />
+        <Route path='/register' element={<RegisterScreen />} />
+        <Route path="*" element={<PageNotFound404 />} />
       </Routes>
     </Router>
   );
