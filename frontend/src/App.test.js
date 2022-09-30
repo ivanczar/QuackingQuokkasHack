e73be6afd80502f919a664aed4dafa1ead376e72
C-Dog346 <callum@clow.net.nz>
2022-10-01 11:26:49 +1300

    2 basic tests done, 1 more complex one on its way

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,20 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders submit button', () => {
   render(<App />);
-  const buttonElement = screen.getByPlaceholderText(/submit button/i)
+  const buttonElement = screen.getByPlaceholderText(/submit button/);
   expect(buttonElement).toBeInTheDocument();
 });
 
-test('renders submit button', () => {
+test('submit button class is "submit-button"', () => {
   render(<App />);
-  const buttonElement = screen.getByPlaceholderText(/submit button/i)
-  expect(buttonElement).toHaveClass(/submit-button/i);
+  const buttonElement = screen.getByPlaceholderText(/submit button/i);
+  expect(buttonElement.className).toBe("submit-button");
 });
 
-// test('renders submit button', () => {
-//   render(<App />);
-//   const buttonElement = screen.getByPlaceholderText(/submit button/i)
-//   expect(buttonElement);
-// });
+test("does not submit when pressing submit after no email", () => {
+  render(<App />);
+  const handleSubmit = jest.fn();
+  const name = screen.getByPlaceholderText("Pet Name*");
+  const email = screen.getByPlaceholderText("Pet Name*");
+
+  fireEvent.change(name, { target: { value: "abc" } });
+  fireEvent.change(email, { target: { value: "abc@abc.com" } });
+
+  fireEvent.click(input, { key: "Enter", code: 13, charCode: 13 });
+
+  expect(handleSubmit).toHaveBeenCalled();
+});
