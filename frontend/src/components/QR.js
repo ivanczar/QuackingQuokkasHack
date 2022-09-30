0ce4dddfcedf6ed48bc88da18fe54c173d31c37d
C-Dog346 <callum@clow.net.nz>
2022-09-30 18:21:42 +1300

    spelling correction

--- frontend/src/components/QR.js
+++ frontend/src/components/QR.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function QR() {
     return (
         <div>
-            <h3>Here is you QR Code</h3>
+            <h3>Here is your QR Code</h3>
             <img src="http://api.qrserver.com/v1/create-qr-code/?data=http://172.20.10.6:3000&amp;size=250x250" alt="" title="" />    
         </div>
     )
