16e285d13737ba8c84b36097d9deff2e334ce3b8
Jacob Tupe <jadatu13@outlook.com>
2022-10-01 10:14:38 +1300

    Removed unused import

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import logo from '../assets/ezyFind_Horizontal.png';
-import '../components/Navbar.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import logo from "../assets/ezyFind_Horizontal.png";
+import "../components/Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
-  let screen = toggle ? 'register' : 'scan';
+  let screen = toggle ? "register" : "scan";
 
   const toggleScreens = () => {
     setToggle(!toggle);
   };
 
   return (
-    <div className='navbar'>
-      <img id='logo' src={logo} alt='ezyFind logo' />
+    <div className="navbar">
+      <img id="logo" src={logo} alt="ezyFind logo" />
       <Link to={`/${screen}`}>
-        <button id='nav-button' onClick={toggleScreens}>
+        <button id="nav-button" onClick={toggleScreens}>
           {screen}
         </button>
       </Link>
